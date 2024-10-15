5e9f09be1cc4cb2060b929a40d119f763319e311
trungvc <kappa@carem.io>
2024-10-16 00:14:40 +0700

    PI-8: update performance

--- src/features/routers/router.tsx
+++ src/features/routers/router.tsx
@@ -1,18 +1,11 @@
 import * as React from "react";
 import { paths } from "./path";
-import { createBrowserRouter, HashRouter, Route, RouterProvider, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Detail from "../../pages/detail";
 import Gallery from "../../pages/gallery";
 
 const Router: React.FC = () => {
   return (
-    // <HashRouter>
-    //   <Routes>
-    //     <Route path={paths.GALLERY} element={<Gallery />} />
-    //     <Route path={paths.PHOTO_DETAIL} element={<Detail />} />
-    //   </Routes>
-    // </HashRouter>
-
     <BrowserRouter basename="Photo_Gallery">
       <Routes>
         <Route path={paths.GALLERY} element={<Gallery />} />
